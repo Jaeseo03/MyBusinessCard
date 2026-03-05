31d8975c2fcbcaa7b66eb4a973ce112d08d22ccf
김재서 <@DESKTOP-8HGU1AT>
2026-03-05 23:54:11 +0900

    초기 커밋.

---
+++ Form1.cs
@@ -0,0 +1,27 @@
+namespace WinFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Random rd = new Random();
+            // 0~255 사이의 R, G, B 값을 무작위로 생성하여 배경색에 적용
+            this.BackColor = Color.FromArgb(rd.Next(256), rd.Next(256), rd.Next(256));
+        }
+    }
+}
